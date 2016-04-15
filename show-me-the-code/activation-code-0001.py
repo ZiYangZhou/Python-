e7994670c2591ed167f07a9d349207a92e7a946c
zhouxu <www.yibinzx@gmail.com>
2016-04-15 08:22:04 +0800

    使用时间生成好像不是太好，删了

--- show-me-the-code/activation-code-0001.py
+++ show-me-the-code/activation-code-0001.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*- 
 __author__ = 'zhouxu'
 '''
 generate activation code
@@ -5,9 +6,11 @@ generate activation code
 import random
 import time
 
-#随机生成ASCCI码为33 - 126的字符作为激活码
-#length为激活码的长度
-#quantity为生成激活码的个数
+# 随机生成ASCCI码为33 - 126的字符作为激活码
+# length为激活码的长度
+# quantity为生成激活码的个数
+
+
 def generate(lenth, quantity):
     ret = []
     for cnt in range(0, quantity):
@@ -17,6 +20,7 @@ def generate(lenth, quantity):
         ret.append(str)
     return ret
 
+
 if __name__ == '__main__':
     ret = generate(15, 4)
-    print ret
+    print 'ret1', ret1
